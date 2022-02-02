5fbe6b67f89dac8ae2c725846b78d123af7a5651
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2022-02-02 09:36:23 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
              }
 
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show_by_id'
+  get '/users/:id', to: 'users#show'
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 
   get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show_by_id'
+  get '/posts/:id', to: 'posts#show'
   resources :comments, :roles, :posts
   #delete '/users/:id', to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
